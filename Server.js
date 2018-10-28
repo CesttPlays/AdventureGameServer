8dceceda2da267212d838bc33a99ce40ad56ba8e
CesttPlays <cesttplays@gmail.com>
2018-10-28 23:14:52 +0100

    Added Simple Login

--- Server.js
+++ Server.js
@@ -5,6 +5,7 @@ var io = require('socket.io')(3000);
 io.on('connection', function(socket) {
     socket.on('Hello',function(data){
         console.log("Se ha conectado: "+ data);
+        socket.broadcast.emit('New Player', data);
     });
     socket.on('Movement',function(data){
        console.log("Moving");
