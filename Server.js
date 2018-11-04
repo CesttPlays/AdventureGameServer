e3823abfdf4b57e834d125dd6ec5578b6e81665d
CesttPlays <cesttplays@gmail.com>
2018-11-04 22:25:42 +0100

    Previous connected players

--- Server.js
+++ Server.js
@@ -1,14 +1,16 @@
 var io = require('socket.io')(3000);
 
-
+var GOArray = new Array();
 
 io.on('connection', function(socket) {
     socket.on('Hello',function(data){
-        console.log("Se ha conectado: "+ data);
+        var stringArray = data.split(",");
+        GOArray.push(stringArray);
         socket.broadcast.emit('New Player', data);
+        socket.emit('Players Connected', GOArray);
     });
     socket.on('Movement',function(data){
-       console.log("Moving");
+      
         socket.broadcast.emit('Movement',data);
     });
 });
