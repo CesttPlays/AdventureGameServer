8b9c5c4dace4b3e2da95e95c1a0332120ad8cf02
CesttPlays <cesttplays@gmail.com>
2019-01-02 01:57:20 +0100

    Fixed data transmision

--- Server.js
+++ Server.js
@@ -1,16 +1,41 @@
-var io = require('socket.io')(3000);
+const io = require('socket.io')(3000);
+const _ = require('lodash')
 
-var GOArray = new Array();
-
+var people = new Array();
+let jsonObject;
 io.on('connection', function(socket) {
+    console.log(socket.id);
+    socket.emit('Connected', socket.id);
     socket.on('Hello',function(data){
-        var stringArray = data.split(",");
-        GOArray.push(stringArray);
-        socket.broadcast.emit('New Player', data);
-        socket.emit('Players Connected', GOArray);
+        try{
+            jsonObject  =JSON.parse(data);
+            console.log(jsonObject.name + " conected");  
+        }catch(err){
+            console.log(err);
+        }
+        socket.broadcast.emit('New Player', JSON.stringify(data));
+        var sString =JSON.stringify(people);
+        socket.emit('Players Connected', sString,function (err, responseData) {
+            if (err) {
+                console.log(err);
+            } else {
+               //emit corecto
+            }
+        }); 
+        people.push(jsonObject);
+        
     });
     socket.on('Movement',function(data){
-      
-        socket.broadcast.emit('Movement',data);
+        try{
+            var jsonObject =JSON.parse(data);
+        }catch(err){
+            console.log(err);
+        }
+        var sString = JSON.stringify(jsonObject);
+        socket.broadcast.emit('Movement',sString);
     });
+    
+    socket.on('error', function (err) {
+    console.log(err);
+});
 });
